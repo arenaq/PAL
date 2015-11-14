66e825696ea6ea695834a8fe377bef3ade8b7e17
Petr Kuška <kuska.petr@gmail.com>
2015-11-14 02:04:15 +0100

    Update CV 6 - Grayův kód, generování podmnožin [solved]

--- src/cv6/GrayCode.java
+++ src/cv6/GrayCode.java
@@ -9,23 +9,24 @@ import java.io.PrintWriter;
  */
 public class GrayCode {
 
-//    static String[] set = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    static final String[] set = {"A", "B", "C", "D", "E", "F"};
+    static char[] set = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
     public static void main(String[] args) throws FileNotFoundException {
         PrintWriter pw = new PrintWriter("subsets.txt");
-        //TODO - gray code increment function http://stackoverflow.com/questions/17490431/gray-code-increment-function
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i < (1 << set.length); i++) { // (1 << set.length) = 2^(set.length)
             int gray = i ^ (i >> 1); // binary to gray code
-            String s = "";
+            sb.delete(0, sb.length());
             int power = 0;
             while (gray > 0) {
                 if ((gray & 1) == 1) {
-                    s = set[power++] + " " + s;
-                    gray = gray >> 1;
+//                    if (sb.length() > 0) sb.append(" "); // takes ~10 sekund
+                    sb.append(set[power]);
                 }
+                power++;
+                gray = gray >> 1;
             }
-            pw.println(s);
+            pw.println(sb.toString());
         }
         pw.close();
     }
