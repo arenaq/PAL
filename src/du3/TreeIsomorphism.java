bf9f7d7f4606ea144cb2a1ad18b5134795ee58f9
Petr Kuška <kuska.petr@gmail.com>
2015-11-17 16:28:47 +0100

    wip

--- src/du3/TreeIsomorphism.java
+++ src/du3/TreeIsomorphism.java
@@ -10,11 +10,11 @@ import java.util.StringTokenizer;
  * @author Petr Kuška
  */
 public class TreeIsomorphism {
-    static short N; // number of nodes of a tree A, max 1200
-    static short[][] A_edges, B_edges;
-    static short[] A_number_of_edges, B_number_of_edges;
-    static short[] A_edge_to_degree, B_edge_to_degree;
-    
+
+    static boolean[][] A_matrix, B_matrix; // {{vertex, vertex}, does they have an edge} - only upper half from diagonal is filled
+    static short[] A_degrees, B_degrees; // {vertex, number of edges}
+    static short[] A_degree_classification, B_degree_classification; // {degree, count} - example {1, 4} means that there are 4 vertices with degree 1
+
     static short[] getDegreesClassification(short[] edge_to_degree) {
         short[] degreesClassification = new short[edge_to_degree.length];
         for (int i = 0; i < edge_to_degree.length; i++) {
@@ -22,92 +22,100 @@ public class TreeIsomorphism {
         }
         return degreesClassification;
     }
-    
+
     static boolean[] getCertificate(short[][] tree) {
         throw new UnsupportedOperationException("Not implemented yet.");
     }
-    
-    static void copyGraphExceptVertex(short[][] out_graph, short[] out_number_of_edges, int vertex) {
-        for (int i = 0; i < vertex; i++) {
-            for (int j = 0; j < B_number_of_edges[i]; j++) {
-                short val = B_edges[i][j];
-                if (val != vertex) {
-                    out_graph[i][out_number_of_edges[i]++] = val;
-                }
-            }
-        }
-        for (int i = vertex+1; i < B_edges.length; i++) {
-            for (int j = 0; j < B_number_of_edges[i]; j++) {
-                short val = B_edges[i][j];
-                if (val != vertex) {
-                    out_graph[i-1][out_number_of_edges[i-1]++] = val;
+
+    static void copyGraphExceptVertex(boolean[][] out_graph, short[] out_degrees, int vertex) {
+        for (int i = 0; i < out_graph.length; i++) {
+            for (int j = i; j < out_graph[i].length; j++) { // limited to diagonal - only upper half of diagonal is filled
+                if (i != vertex && j != vertex) {
+                    out_graph[i][j] = B_matrix[i][j];
+                    out_degrees[i]++;
+                    out_degrees[j]++;
                 }
             }
         }
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        N = Short.parseShort(in.readLine());
-        
-        A_edges = new short[N][N];
-        A_number_of_edges = new short[N];
-        A_edge_to_degree = new short[N];
-        for (int i = 0; i < N-1; i++) {
-             StringTokenizer st = new StringTokenizer(in.readLine());
-             short v1 = Short.parseShort(st.nextToken());
-             short v2 = Short.parseShort(st.nextToken());
-             A_edges[v1][A_number_of_edges[v1]++] = v2;
-             A_edges[v2][A_number_of_edges[v2]++] = v1;
-             A_edge_to_degree[v1]++;
-             A_edge_to_degree[v2]++;
+        short N = Short.parseShort(in.readLine()); // number of nodes of a tree A, max 1200
+
+        A_matrix = new boolean[N][N];
+        A_degrees = new short[N];
+        for (int i = 0; i < N - 1; i++) {
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            short v1 = Short.parseShort(st.nextToken());
+            short v2 = Short.parseShort(st.nextToken());
+            if (v1 < v2) {
+                A_matrix[v1][v2] = true;
+            } else {
+                A_matrix[v2][v1] = true;
+            }
+            A_degrees[v1]++;
+            A_degrees[v2]++;
         }
-        
-        B_edges = new short[N+1][N+1];
-        B_number_of_edges = new short[N+1];
-        B_edge_to_degree = new short[N+1];
+
+        B_matrix = new boolean[N + 1][N + 1];
+        B_degrees = new short[N + 1];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(in.readLine());
-             short v1 = Short.parseShort(st.nextToken());
-             short v2 = Short.parseShort(st.nextToken());
-             B_edges[v1][B_number_of_edges[v1]++] = v2;
-             B_edges[v2][B_number_of_edges[v2]++] = v1;
-             B_edge_to_degree[v1]++;
-             B_edge_to_degree[v2]++;
+            short v1 = Short.parseShort(st.nextToken());
+            short v2 = Short.parseShort(st.nextToken());
+            if (v1 < v2) {
+                B_matrix[v1][v2] = true;
+            } else {
+                B_matrix[v2][v1] = true;
+            }
+            B_degrees[v1]++;
+            B_degrees[v2]++;
         }
-        
+
         System.out.println("Graph B:");
-        printGraph(B_edges, B_number_of_edges);
-        for (int i = 0; i < B_edge_to_degree.length; i++) {
-            short[][] graph_candidate = new short[N][N];
-            short[] number_of_edges = new short[N];
-            if (B_edge_to_degree[i] == 1) {
-                copyGraphExceptVertex(graph_candidate, number_of_edges, i);
-                // VYPIS
+        printGraph(B_matrix, B_degrees);
+        for (int i = 0; i < B_degrees.length; i++) {
+            if (B_degrees[i] == 1) {
                 System.out.println("Vertex "+i+":");
-                printGraph(graph_candidate, number_of_edges);
+                for (int x = 0; x < B_matrix.length; x++) {
+                    for (int y = x; y < B_matrix[x].length; y++) { // limited to diagonal - only upper half of diagonal is filled
+                        if (x != i && y != i) {
+                            if (B_matrix[x][y] == true) System.out.println(x+" "+y);
+                        }
+                    }
+                }
+                System.out.println("-----------------");
             }
         }
         // TODO
         /* pro vsechny vrcholy X stupne 1 z grafu B:
-        pokud podgraf G grafu B vytvoreny odebranim hrany vrcholu X ma stejne schema stupnu vrcholu,   tak vytvorime z grafu G certifikat a porovname jej s grafem A
-        */
+         pokud podgraf G grafu B vytvoreny odebranim hrany vrcholu X ma stejne schema stupnu vrcholu,   tak vytvorime z grafu G certifikat a porovname jej s grafem A
+         */
     }
-    
-    static void printGraph(short[][] graph, short[] number_of_edges) {
+
+    static void printGraph(boolean[][] graph, short[] number_of_edges) {
         StringBuilder sb = new StringBuilder("\n");
         for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < number_of_edges[i]; j++) {
-                sb.append(i+" "+graph[i][j]+"\n");
+            for (int j = i; j < graph[i].length; j++) {
+                if (graph[i][j] == true) {
+                    sb.append(i);
+                    sb.append(" ");
+                    sb.append(j);
+                    sb.append("\n");
+                }
             }
         }
         System.out.println(sb.toString());
     }
-    
+
     static void print1dArray(short[] array) {
         StringBuilder sb = new StringBuilder("\n");
         for (int i = 0; i < array.length; i++) {
-            sb.append(i+":"+array[i]+"\n");
+            sb.append(i);
+            sb.append(":");
+            sb.append(array[i]);
+            sb.append("\n");
         }
         System.out.println(sb.toString());
     }
