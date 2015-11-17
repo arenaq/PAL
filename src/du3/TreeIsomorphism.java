b82fea5c474457a01e797171daf28a3def978f41
Petr Kuška <kuska.petr@gmail.com>
2015-11-17 13:27:10 +0100

    Update DU 3 (refactoring)

--- src/du3/TreeIsomorphism.java
+++ src/du3/TreeIsomorphism.java
@@ -13,7 +13,7 @@ public class TreeIsomorphism {
     static short N; // number of nodes of a tree A, max 1200
     static short[][] A_edges, B_edges;
     static short[] A_number_of_edges, B_number_of_edges;
-    static short[] A_degree, B_degree;
+    static short[] A_edge_to_degree, B_edge_to_degree;
     
     static short[] getCertificate(short[][] tree) {
         throw new UnsupportedOperationException("Not implemented yet.");
@@ -23,30 +23,30 @@ public class TreeIsomorphism {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         N = Short.parseShort(in.readLine());
         
-        A_edges = new short[N][N]; // TODO rozměry pole?
+        A_edges = new short[N][N];
         A_number_of_edges = new short[N];
-        A_degree = new short[N];
+        A_edge_to_degree = new short[N];
         for (int i = 0; i < N-1; i++) {
              StringTokenizer st = new StringTokenizer(in.readLine());
              short v1 = Short.parseShort(st.nextToken());
              short v2 = Short.parseShort(st.nextToken());
              A_edges[v1][A_number_of_edges[v1]++] = v2;
              A_edges[v2][A_number_of_edges[v2]++] = v1;
-             A_degree[v1]++;
-             A_degree[v2]++;
+             A_edge_to_degree[v1]++;
+             A_edge_to_degree[v2]++;
         }
         
         B_edges = new short[N+1][N+1];
         B_number_of_edges = new short[N+1];
-        B_degree = new short[N+1];
+        B_edge_to_degree = new short[N+1];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(in.readLine());
              short v1 = Short.parseShort(st.nextToken());
              short v2 = Short.parseShort(st.nextToken());
              B_edges[v1][B_number_of_edges[v1]++] = v2;
              B_edges[v2][B_number_of_edges[v2]++] = v1;
-             B_degree[v1]++;
-             B_degree[v2]++;
+             B_edge_to_degree[v1]++;
+             B_edge_to_degree[v2]++;
         }
         // TODO
         /* pro vsechny vrcholy X stupne 1 z grafu B:
@@ -73,11 +73,11 @@ public class TreeIsomorphism {
     static void printDegrees() {
         StringBuilder sb = new StringBuilder("\n");
         for (int i = 0; i < N; i++) {
-            sb.append(i+":"+A_degree[i]+"\n");
+            sb.append(i+":"+A_edge_to_degree[i]+"\n");
         }
         sb.append("-\n");
         for (int i = 0; i < N+1; i++) {
-            sb.append(i+":"+B_degree[i]+"\n");
+            sb.append(i+":"+B_edge_to_degree[i]+"\n");
         }
         System.out.println(sb.toString());
     }
